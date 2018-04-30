d26da057baf443d4c7376fa1f2e6659b1c270d3d
Rafael Stern <rbstern@gmail.com>
2018-04-30 15:13:54 -0300

    KL normal unknown sigma
    First run.

--- Normal_KL.R
+++ Normal_KL.R
@@ -12,17 +12,34 @@ mu0 = 0
 mus = seq(from = -1, to = 1, length.out = n)
 sigmas = seq(from = 1/n, to = 2, length.out = n)
 
-sigma0 = 1
-grid <- tibble()
-for(sigma1 in sigmas)
+sigma_grid = list()
+for(sigma0 in sigmas)
 {
-  radius = sigma1*(1+2*eps-sigma0/sigma1-log(sigma1/sigma0))
-  inter_left = mu0 - radius
-  inter_right = mu0 + radius
-  color = (mus > inter_left) & (mus < inter_right)
-  grid %<>% rbind(tibble(mu = mus, sigma = sigma1, color = color))
+  new_grid <- tibble()
+  for(sigma1 in sigmas)
+  {
+    radius = sigma1*(1+2*eps-sigma0/sigma1-log(sigma1/sigma0))
+    inter_left = mu0 - radius
+    inter_right = mu0 + radius
+    color = (mus > inter_left) & (mus < inter_right)
+    new_grid %<>% rbind(tibble(mu = mus, sigma = sigma1, color = color))
+  }
+  sigma_grid[[as.character(sigma0)]] = new_grid
 }
 
-grid %>% 
-  ggplot(aes(x = mu, y = sigma, color = color)) +
+#write_rds(sigma_grid, "./data/KL_norm_0.1_100_0.rds")
+#sigma_grid = sigma_grid = read_rds("./data/KL_norm_0.1_100_0.rds")
+
+small_grid = sigma_grid[[round(n/2)]]
+join_grid = tibble(mu = small_grid$mu, sigma = small_grid$sigma)
+max_col = sigma_grid[[1]]$color
+for(sigma0 in as.character(sigmas))
+{
+  max_col = (max_col | sigma_grid[[sigma0]]$color)
+}
+join_grid$color = max_col
+
+join_grid %>% 
+  filter(color == TRUE) %>%
+  ggplot(aes(x = mu, y = sigma)) +
   geom_point()
